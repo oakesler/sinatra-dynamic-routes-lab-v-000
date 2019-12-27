cbd9abdbfe627899dae96e0ad88e7257d641cf6a
Olivia Kesler <oakesler@protonmail.ch>
2019-12-27 00:43:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,7 +28,20 @@ class App < Sinatra::Base
   end
   
   get "/:operation/:number1/:number2" do
-    if 
-  
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    if @operation == "add"
+      "#{@number1 + @number2}"
+      elsif @operation == "subtract"
+      "#{@number1 - @number2}"
+      elsif @operation == "multiply"
+      "#{@number1 * @number2}"
+      elsif @operation == "divide"
+      "#{@number1 % @number2}"
+    else
+      "Error"
+    end
+  end 
   
 end
